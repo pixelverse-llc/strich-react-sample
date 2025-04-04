430a72614d9dc8a25234a5e8b6117da62e87b35c
Alex Suzuki <alex@pixelverse.ch>
2025-04-04 10:40:13 +0200

    build: allow vite to respond to requests from all hosts (support ngrok and other tools)

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    // Allow all hosts To support using tools like ngrok during development
+    allowedHosts: true
+  },
   plugins: [react()],
 })
