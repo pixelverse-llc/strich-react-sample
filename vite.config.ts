b43d72d7905bc064c9dc2f3b824413a31dbfd731
Alex Suzuki <alex@pixelverse.ch>
2025-04-04 10:41:40 +0200

    build: add comment regarding vite.allowedHosts directive

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
+
     // Allow all hosts To support using tools like ngrok during development
+    // Caveats apply: https://vite.dev/config/server-options.html#server-allowedhosts
+    // Remove if not needed, or replace with domain, e.g. ['tunnel.mycorp.com']
     allowedHosts: true
   },
   plugins: [react()],
