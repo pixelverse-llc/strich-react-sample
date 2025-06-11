32b7f7c1490b6fb081149538dc1049fa8d63ed38
Alex Suzuki <alex@pixelverse.ch>
2025-06-11 11:47:00 +0200

    feat: added PopupScanner example

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,8 +1,28 @@
 import { Link } from 'react-router-dom';
+import { useState } from 'react';
+
+import { StrichSDK, PopupScanner } from '@pixelverse/strichjs-sdk';
+import { sdkLicenseKey } from '../scanner-config.ts';
 
 import './home.css'
 
 function Home() {
+
+    const [scannedCode, setScannedCode] = useState<string>();
+
+    /**
+     * Scan barcodes using the PopupScanner, see: https://docs.strich.io/the-popup-scanner.html
+     */
+    async function scanWithPopup() {
+        if (!StrichSDK.isInitialized()) {
+            await StrichSDK.initialize(sdkLicenseKey());
+        }
+        const detections = await PopupScanner.scan({ symbologies: ['code128', 'qr'] });
+        if (detections) {
+            setScannedCode(detections[0].data);
+        }
+    }
+
     return (
         <main className='home-container'>
             <header>
@@ -23,6 +43,12 @@ function Home() {
                 <header><h3>Multiple Scans</h3></header>
                 <p>Scan continuously until a number of distinct barcodes are scanned.</p>
                 <Link to={`/multiple`}>Start Multiple</Link>
+            </section>
+            <section>
+                <header><h3>PopupScanner</h3></header>
+                <p>Scan using the built-in PopupScanner UI.</p>
+                <button onClick={scanWithPopup}>Scan with Popup</button>
+                <p>Scanned code: {scannedCode}</p>
             </section>
         </main>
     )
